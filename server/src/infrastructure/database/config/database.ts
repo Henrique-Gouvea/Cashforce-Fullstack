a42f53569e6a4d6c37c79a6e42f42f71cce9997e
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-27 18:03:01 -0300

    feat(Database): Config: Create config for database

---
+++ server/src/infrastructure/database/config/database.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { Dialect, Options } from 'sequelize';
+
+const config: Options = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'NF_DATABASE',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DIALECT as Dialect,
+    logging: false,
+};
+
+export = config;
