4bc4ccf1371c96895a9e17316a273d6c079cc3ac
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-27 17:49:16 -0300

    build: Install morgan, helmet and cors

--- server/src/application/app.ts
+++ server/src/application/app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+    res.send('hello, world!')
+})
 
 export default app;
