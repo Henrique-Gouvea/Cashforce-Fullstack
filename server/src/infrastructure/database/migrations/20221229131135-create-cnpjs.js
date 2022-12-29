22ff08d74025be65f6729b1e6b59d5b10e124b52
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-29 10:35:45 -0300

    feat(Migrations): Configured migration dor cnpjs

--- server/src/infrastructure/database/migrations/20221229131135-create-cnpjs.js
+++ server/src/infrastructure/database/migrations/20221229131135-create-cnpjs.js
@@ -1,22 +1,32 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('cnpjs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      cnpj: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      companyType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('cnpjs');
   }
 };
