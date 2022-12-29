8ab5304aaa308ac58f0bff26366d5080af6a6014
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-29 10:30:06 -0300

    feat(Migrations): Configured migration users

--- server/src/infrastructure/database/migrations/20221229131117-create-users.js
+++ server/src/infrastructure/database/migrations/20221229131117-create-users.js
@@ -1,22 +1,57 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      mobile: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      departament: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      verificationCode: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      emailChecked: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      cashforceAdm: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
   }
 };
