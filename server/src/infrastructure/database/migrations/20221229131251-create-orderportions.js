98cfa2096ae6bfe479183e8533f33b54934f2b51
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-29 10:52:38 -0300

    feat(Migrations): COnfigured migration orderportions

--- server/src/infrastructure/database/migrations/20221229131251-create-orderportions.js
+++ server/src/infrastructure/database/migrations/20221229131251-create-orderportions.js
@@ -1,22 +1,44 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      nDup: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      dVenc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      vDup: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      availableToMarket: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+      },
+      orderId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('orderportions');
   }
 };
