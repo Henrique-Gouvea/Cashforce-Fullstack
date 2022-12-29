ae8fd1b74573ce0e70e25654395228df908817e7
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-29 08:25:07 -0300

    feat(Interfaces): Create interface for Users

---
+++ server/src/infrastructure/interfaces/database/IUsers.ts
@@ -0,0 +1,10 @@
+export interface IUsers {
+  name: string,
+  email: string,
+  phoneNumber: string,
+  mobile: string,
+  departament: string,
+  verificationCode: string,
+  emailChecked: string,
+  cashforceAdm: number,
+}
