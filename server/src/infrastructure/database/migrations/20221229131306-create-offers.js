9414d7c546c03f9122a823db4bc63dc6f00f543a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-29 10:54:51 -0300

    feat(Migrations): Configured migrations for offers

--- server/src/infrastructure/database/migrations/20221229131306-create-offers.js
+++ server/src/infrastructure/database/migrations/20221229131306-create-offers.js
@@ -1,22 +1,64 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('offers', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      tax: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      tariff: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      adValorem: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      float: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      iof: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      expiresIn: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      paymentStatusSponsor: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+      },
+      paymentStatusProvider: {
+        type: DataTypes.TINYINT,
+        allowNull: true,
+      },
+      orderId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      sponsorId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('offers');
   }
 };
