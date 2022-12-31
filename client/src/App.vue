a4fe34b2b781e97f8ed339292da859c60861b622
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-12-31 08:41:31 -0300

    feat: Starting front end Vue

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TableGeneric msg="Welcome to Table" />
+</template>
+
+<script>
+  import TableGeneric from './components/Table.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      TableGeneric
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
