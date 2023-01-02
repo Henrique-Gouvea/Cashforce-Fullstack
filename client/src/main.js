3d8389a2fef31e6e15c4f673d71c8517cff6039d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-02 08:42:39 -0300

    build: Install bootstrap-vue and configure in main

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+	render: h => h(App)
+}).$mount('#app')
+// createApp(App).mount('#app')
