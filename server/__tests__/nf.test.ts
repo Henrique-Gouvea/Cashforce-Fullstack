4f005731cc320cd36db290d29db0248d52094618
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-03 19:27:15 -0300

    build: Instll chai for test

--- server/__tests__/nf.test.ts
+++ server/__tests__/nf.test.ts
@@ -1,5 +1,5 @@
 import * as sinon from 'sinon';
-import * as chai from 'chai';
+import { expect, request, use } from 'chai';
 import '@types/jest';
 import chaiHttp from 'chai-http';
 import app from '../src/application/app';
@@ -7,9 +7,7 @@ import app from '../src/application/app';
 import { listNF } from './mocks/nf'
 import { SequelizeOrders } from '../src/infrastructure/database/models/Orders';
 
-chai.use(chaiHttp);
-
-const { expect } = chai;
+use(chaiHttp);
 
 describe('Teste NF', () => {
     beforeEach(() => {
@@ -19,7 +17,7 @@ describe('Teste NF', () => {
     it('Deve retornar a lista com todas as NFs', async () => {
         sinon.stub(SequelizeOrders, 'findAll').resolves(listNF);
 
-        const response = await chai.request(app).get('/user/nfs');
+        const response = await request(app).get('/user/nfs');
 
         expect(response.status).to.equal(200);
         expect(response.body).to.equal(listNF);
