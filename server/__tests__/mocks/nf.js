ef6e4f3dd99a02cd67f3d4b9f71bccd785e24ea4
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-03 19:16:21 -0300

    test(Controller): create test in route NF

---
+++ server/__tests__/mocks/nf.js
@@ -0,0 +1,58 @@
+const listNF = [
+  {
+    "orderNfId": "1605181324132",
+    "nNf": "18153",
+    "orderNumber": "18153",
+    "emissionDate": "2020-10-30T11:00:00-03:00",
+    "value": "198450",
+    "orderStatusBuyer": "Pendente de confirmação",
+    "buyer": {
+      "name": "SACADO 001",
+      "tradingName": "SACADO 001 LTDA"
+    },
+    "provider": {
+      "name": "CEDENTE 002",
+      "tradingName": "CEDENTE 002 LTDA",
+      "responsibleEmail": null,
+      "phoneNumber": null
+    }
+  },
+  {
+    "orderNfId": "160518132413",
+    "nNf": "18157",
+    "orderNumber": "18157",
+    "emissionDate": "2020-11-04T15:32:35-02:00",
+    "value": "168850",
+    "orderStatusBuyer": "Pendente de confirmação",
+    "buyer": {
+      "name": "SACADO 001",
+      "tradingName": "SACADO 001 LTDA"
+    },
+    "provider": {
+      "name": "CEDENTE 002",
+      "tradingName": "CEDENTE 002 LTDA",
+      "responsibleEmail": null,
+      "phoneNumber": null
+    }
+  },
+  {
+    "orderNfId": "1605181324130",
+    "nNf": "18184",
+    "orderNumber": "18184",
+    "emissionDate": "2020-11-10",
+    "value": "222795",
+    "orderStatusBuyer": "Recebida e confirmada",
+    "buyer": {
+      "name": "SACADO 001",
+      "tradingName": "SACADO 001 LTDA"
+    },
+    "provider": {
+      "name": "CEDENTE 002",
+      "tradingName": "CEDENTE 002 LTDA",
+      "responsibleEmail": null,
+      "phoneNumber": null
+    }
+  }
+]
+
+export { listNF }
---
+++ server/__tests__/nf.test.ts
@@ -0,0 +1,29 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import '@types/jest';
+import chaiHttp from 'chai-http';
+import app from '../src/application/app';
+
+import { listNF } from './mocks/nf'
+import { SequelizeOrders } from '../src/infrastructure/database/models/Orders';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste NF', () => {
+    beforeEach(() => {
+        sinon.restore();
+    });
+
+    it('Deve retornar a lista com todas as NFs', async () => {
+        sinon.stub(SequelizeOrders, 'findAll').resolves(listNF);
+
+        const response = await chai.request(app).get('/user/nfs');
+
+        expect(response.status).to.equal(200);
+        expect(response.body).to.equal(listNF);
+
+    });
+
+});
